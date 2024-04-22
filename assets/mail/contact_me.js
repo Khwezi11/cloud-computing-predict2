e41b6007450151c4bd3249c9392b74984d4adc0b
khwezi11 <khwezizama@gmail.com>
2024-04-23 01:10:01 +0200

    API endpoint URL added to contact_me.js file

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://gwnd2tjn27.execute-api.eu-west-1.amazonaws.com/First",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
